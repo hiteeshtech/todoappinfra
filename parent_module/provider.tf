035b8fbb465335762d3947f2d94d1236b188c5f7
sharmah91 <sharmah91@gmail.com>
2025-09-26 16:36:51 +0530

    backend block removed

--- parent_module/provider.tf
+++ parent_module/provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~>4.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "RG_terraform_hitesh_init"
-    storage_account_name = "hiteshstginit"
-    container_name       = "hiteshinitcontainer"
-    key                  = "terraform.tfstate"
-  }
-
 }
 
 
